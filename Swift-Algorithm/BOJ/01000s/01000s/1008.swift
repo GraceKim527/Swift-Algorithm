7c8d3250d46933e0e0b57c6fbad3d04b824a9852
gracekim527 <lemonherb0323@naver.com>
2024-06-16 20:56:27 +0900

    [BOJ] 1008 A / B / Bronze 5

---
+++ Swift-Algorithm/BOJ/01000s/01000s/1008.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/23/24.
+//
+
+import Foundation
+
+let line = readLine()!
+let lineArr = line.split(separator: " ");
+
+let a = Double(lineArr[0])!
+let b = Double(lineArr[1])!
+
+
+print(a / b)
