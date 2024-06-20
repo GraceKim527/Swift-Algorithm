090d4ad5c49aef4962d65f34925181b7bd327702
gracekim527 <lemonherb0323@naver.com>
2024-06-20 12:50:23 +0900

    [BOJ] 1001 A - B / Bronze 5

---
+++ Swift-Algorithm/BOJ/01000s/1001.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/23/24.
+//
+
+import Foundation
+
+let line = readLine()!
+let lineArr = line.split(separator: " ")
+
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+
+print(a - b)
