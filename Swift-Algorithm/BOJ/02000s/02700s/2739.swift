3fb36ab9af8c4ac8fd593ca52dded68583a7b28f
gracekim527 <lemonherb0323@naver.com>
2024-06-25 03:46:15 +0900

    :white_check_mark: [BOJ] 2739 구구단 / Bronze 5

---
+++ Swift-Algorithm/BOJ/02000s/02700s/2739.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/25/24.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+for i in 1..<10 {
+    print("\(n) * \(i) = \(n * i)")
+}
