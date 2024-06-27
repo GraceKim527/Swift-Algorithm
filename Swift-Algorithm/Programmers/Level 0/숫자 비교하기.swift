7fa6ebc88e629c63e47b1f0a028027d387b0b78a
gracekim527 <lemonherb0323@naver.com>
2024-06-27 23:43:26 +0900

    :white_check_mark: [PGS] 숫자 비교하기 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/숫자 비교하기.swift
@@ -0,0 +1,14 @@
+//
+//  숫자 비교하기.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/27/24.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    
+    guard num1 == num2 else { return -1 }
+    return 1
+}
