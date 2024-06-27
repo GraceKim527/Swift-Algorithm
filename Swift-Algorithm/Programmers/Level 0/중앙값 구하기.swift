62353c2ea7880c7bce54a2713b2618cafb8abb19
gracekim527 <lemonherb0323@naver.com>
2024-06-28 00:29:42 +0900

    :white_check_mark: [PGS] 중앙값 구하기 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/중앙값 구하기.swift
@@ -0,0 +1,15 @@
+//
+//  중앙값 구하기.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/28/24.
+//
+
+import Foundation
+
+func solution(_ array:[Int]) -> Int {
+    var tmp = array
+    tmp.sort() // 오름차순 정렬
+    
+    return tmp[tmp.count / 2]
+}import Foundation
