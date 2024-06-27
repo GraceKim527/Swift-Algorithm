4704a55b8f613f1180c5f8ef01ecde22830f0cb1
gracekim527 <lemonherb0323@naver.com>
2024-06-28 01:01:47 +0900

    :white_check_mark: [PGS] 아이스 아메리카노 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/아이스 아메리카노.swift
@@ -0,0 +1,12 @@
+//
+//  아이스 아메리카노.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/28/24.
+//
+
+import Foundation
+
+func solution(_ money:Int) -> [Int] {
+    return [money / 5500, money%5500]
+}
