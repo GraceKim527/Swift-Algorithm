205dc56823062b5ca5b27a2dc518b764d0aae819
gracekim527 <lemonherb0323@naver.com>
2024-06-27 23:50:53 +0900

    :white_check_mark: [PGS] 두 수의 합 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/두 수의 합.swift
@@ -0,0 +1,12 @@
+//
+//  두 수의 합.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/27/24.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 + num2
+}
