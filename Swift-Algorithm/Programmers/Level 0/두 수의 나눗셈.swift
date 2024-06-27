3d11840d613c027d4d68989dd4110a38af3c2984
gracekim527 <lemonherb0323@naver.com>
2024-06-28 00:14:36 +0900

    :white_check_mark: [PGS] 두 수의 나눗셈 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/두 수의 나눗셈.swift
@@ -0,0 +1,12 @@
+//
+//  두 수의 나눗셈.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/28/24.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return Int((Double(num1)/Double(num2))*1000)
+}
