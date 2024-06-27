0db7b99ed6a6862fd1029f809e582302e00b2619
gracekim527 <lemonherb0323@naver.com>
2024-06-27 23:45:13 +0900

    :white_check_mark: [PGS] 나머지 구하기 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/나머지 구하기.swift
@@ -0,0 +1,12 @@
+//
+//  나머지 구하기.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/27/24.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 % num2
+}
