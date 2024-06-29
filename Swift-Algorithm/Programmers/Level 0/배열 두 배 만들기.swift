8f68521dd725b468712116414adb311447ed12b7
gracekim527 <lemonherb0323@naver.com>
2024-06-29 14:48:15 +0900

    :white_check_mark: [PGS] 배열 두 배 만들기 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/배열 두 배 만들기.swift
@@ -0,0 +1,12 @@
+//
+//  배열 두 배 만들기.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 6/29/24.
+//
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    return numbers.map{$0 * 2}
+}
