9d24f6337431f88822baa17956b0a08f0558d261
gracekim527 <lemonherb0323@naver.com>
2024-07-05 01:44:54 +0900

    :white_check_mark: [PGS] 최빈값 구하기 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/최빈값 구하기.swift
@@ -0,0 +1,31 @@
+//
+//  최빈값 구하기.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 7/5/24.
+//
+
+import Foundation
+
+func solution(_ array:[Int]) -> Int {
+    var dictionary = [Int: Int]()
+    var keyNum = 0
+    var count = 0
+    
+    for i in array {
+        if dictionary.contains {$0.key == i} {
+            dictionary[i]! += 1
+        } else {
+            dictionary[i] = 1
+        }
+    }
+    
+    for (i, j) in dictionary {
+        if j == dictionary.values.max() {
+            count += 1
+            keyNum = i
+        }
+    }
+    
+    return count == 1 ? keyNum : -1
+}
