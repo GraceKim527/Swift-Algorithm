3a89691ebd8dd54f6d7f14ad9f1afbc3cb7f7072
gracekim527 <lemonherb0323@naver.com>
2024-07-04 18:06:02 +0900

    :white_check_mark: [PGS] 최댓값 만들기 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/최댓값 만들기.swift
@@ -0,0 +1,12 @@
+//
+//  최댓값 만들기.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 7/4/24.
+//
+
+import Foundation
+
+func solution(_ my_string:String, _ letter:String) -> String {
+    return my_string.replacingOccurrences(of: letter, with: "")
+}
