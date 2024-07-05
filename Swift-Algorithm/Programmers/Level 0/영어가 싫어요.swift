ff3c898ebac28d010524a42d04c920d5197e0bae
gracekim527 <lemonherb0323@naver.com>
2024-07-05 22:04:02 +0900

    :white_check_mark: [PGS] 영어가 싫어요 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/영어가 싫어요.swift
@@ -0,0 +1,16 @@
+//
+//  영어가 싫어요.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 7/5/24.
+//
+
+import Foundation
+
+func solution(_ numbers:String) -> Int64 {
+    var numbers = numbers
+    var string_num = ["zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"]
+    for i in string_num {
+        numbers = numbers.replacingOccurrences(of: i.key, with: String(i.value))
+    }
+    return Int64(numbers)!}
