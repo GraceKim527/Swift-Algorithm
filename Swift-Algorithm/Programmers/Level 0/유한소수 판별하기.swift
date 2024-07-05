9b1731f5a49f05eaf75b91715eb6c733171129e1
gracekim527 <lemonherb0323@naver.com>
2024-07-06 02:41:27 +0900

    :white_check_mark: [PGS] 유한소수 판별하기 / Level 0

---
+++ Swift-Algorithm/Programmers/Level 0/유한소수 판별하기.swift
@@ -0,0 +1,31 @@
+//
+//  유한소수 판별하기.swift
+//  Swift-Algorithm
+//
+//  Created by 김은혜 on 7/6/24.
+//
+
+import Foundation
+
+func gcd(a: Int, b: Int) -> Int{
+    if a % b == 0 {
+        return b
+    }
+    return gcd(a: b, b: a % b)
+}
+
+func solution(_ a:Int, _ b:Int) -> Int {
+    var bNum = b / gcd(a: a, b: b)
+    var temp = bNum
+    var result = [Int]()
+    while temp != 1 {
+        for i in 2... {
+            if temp % i == 0 {
+                result.append(i)
+                temp = temp / i
+                break
+            }
+        }
+    }
+    return result.allSatisfy { $0 == 2 || $0 == 5 } ? 1 : 2
+}
